6e23ea642ebd62da9127e8d28e7bd70cb8c47ad0
RiteshDhongade <ritesh_d37@yahoo.com>
2023-07-22 23:19:10 +0530

    Menu_List_of_List

---
+++ Menu_List_of_lists.py
@@ -0,0 +1,5 @@
+menu = [['Egg Sandwich','Bagel','Coffee'],['BLT','PB&J','Turkey Sandwich'],['Soup','Salad','Spaghetti','Taco']]
+# print('Brakefast Menu:\t', menu[0])
+# print('Lunch Menu:\t',menu[1])
+# print('Dinner Menu:\t',menu)
+print(menu[0][1])
