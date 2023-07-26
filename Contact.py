37b9e811346c814522c7548ce0f34acbd6756650
RiteshDhongade <ritesh_d37@yahoo.com>
2023-07-26 23:39:04 +0530

    Contacts

---
+++ Contact.py
@@ -0,0 +1,19 @@
+#need to make group of email.
+contact ={
+    "number":4,
+    "students":
+    [
+        {"name":"Sarah Holderness","email":"sarah@example.com"},{"name":"Ritesh Dhongade","name":"Ritesh Dhongade","email":"ritesh@example.com"}
+
+    ]
+}
+# for student in contact["students"]:
+#     print(student)
+
+
+
+#For email only
+for student in contact["students"]:
+    print(student["email"]) 
+
+
