4d77921786fdfccae8740bb05cd610b9e88947fb
RiteshDhongade <ritesh_d37@yahoo.com>
2023-07-26 23:39:53 +0530

    Expense_Input

---
+++ Expense_Input.py
@@ -0,0 +1,7 @@
+total=0
+expenses=[]
+num_expenses=int(input("enter # of expenses: "))
+for i in range(num_expenses):
+    expenses.append(float(input("Enter an expenses: ")))
+total=sum(expenses)
+print("You spent $", total,sep='')
