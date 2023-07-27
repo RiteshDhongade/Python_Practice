29d0452a4ea85c1e3c1b2a3a8afa154c28ec0603
RiteshDhongade <ritesh_d37@yahoo.com>
2023-07-27 22:29:30 +0530

    Movie_Schedule

---
+++ movie_schedule.py
@@ -0,0 +1,11 @@
+current_movies = {'The Grinch':'11:00 am', 'Rudolph':'1:00 pm','Frosty the snowman':'3:00 pm','Christmas Vacation':'5:00 pm'}
+print("We're showing the following movies: ")
+for key in current_movies:
+    print(key)
+movie= input('What movie would you like the schedule for?\n' )
+showtime = current_movies.get(movie)
+if showtime == None:
+    print("Requested showtime isn't playing")
+else:
+    print(movie," is playing at ", showtime )
+    
