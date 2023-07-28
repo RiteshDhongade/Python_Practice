82d34ecbf1fbc6732fa6078d5c95097a2f7e031e
RiteshDhongade <ritesh_d37@yahoo.com>
2023-07-28 23:31:52 +0530

    Translate_using_Dictionary

---
+++ Translate-using-Dictionary.py
@@ -0,0 +1,5 @@
+acronyms = {'LOL' : 'laugh out loud', 'IDK' : "I don't know", 'TBH' : 'to be honest'}
+sentence = 'IDK' + ' what happened ' + 'TBH'
+translation = acronyms.get('IDK') + ' what happened ' + acronyms.get('TBH')
+print('Sentence: ', sentence)
+print('translation: ', translation)
