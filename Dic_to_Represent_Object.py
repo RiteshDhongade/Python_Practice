d1959bb22d035b9157226784b3a21ce061ba74a0
RiteshDhongade <ritesh_d37@yahoo.com>
2023-07-28 23:32:42 +0530

    Dic_to_represent_object

---
+++ Dic_to_Represent_Object.py
@@ -0,0 +1,3 @@
+#we want to reperent their attributes such as their name, age, and city. Use dic to save Key:value pair.
+person={'name':'Sarah Smith','City':'Mumbai','age':'26'}
+print(person.get('name'), 'is',person.get('age'),'year old','from', person.get('City'),'city.')
