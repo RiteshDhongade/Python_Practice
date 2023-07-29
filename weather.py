f25337c59d8da091c0f91e969898930f877a1235
RiteshDhongade <ritesh_d37@yahoo.com>
2023-07-29 23:31:07 +0530

    Weather

---
+++ weather.py
@@ -0,0 +1,17 @@
+import requests
+
+api_key = "575d75949f3ba05bfb993101b034c596"
+city = "Nagpur"
+url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
+request = requests.get(url)
+json = request.json()
+#print(json)
+description = json.get("weather")[0].get("description")
+print("Today's forcast is", description)
+temp_min = json.get("main").get("temp_min")
+print("Minimum temperatur is", temp_min)
+temp_max = json.get("main").get("temp_max")
+print("Maximum temperatur is", temp_max)
+coord_lon = json.get("coord").get("lon")
+coord_lat = json.get("coord").get("lat")
+print(city," longitute is", coord_lon," and latitude is", coord_lat)
