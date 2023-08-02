289fbad931b6807781586a484d9b61a46e4d2b51
RiteshDhongade <ritesh_d37@yahoo.com>
2023-08-02 23:59:16 +0530

    Multipe_inheritance

--- Multiple_inheritance.py
+++ Multiple_inheritance.py
@@ -2,10 +2,9 @@ class class1:
     def test_class1(self):
         print("this is class 1")
 class class2:
-    def test_class1(self):
+    def test_class2(self):
         print("this is class 2")  
 class class3 (class1 , class2):
     pass
 obj_class3 = class3()
-obj_class3.test_class1()
-              
+obj_class3.test_class2()
