8e9d4d926c19dd412d73231bd3316de92901ab5f
RiteshDhongade <ritesh_d37@yahoo.com>
2023-08-05 23:54:25 +0530

    Decorator_new

--- Decorator.py
+++ Decorator.py
@@ -1,1 +1,11 @@
-#Decorator
+def deco(func):
+    def inner_deco():
+        print("This is start of function")
+        func()
+        print("This is end of function")
+    return inner_deco
+@deco
+def test1():
+    print(4+5)
+
+test1()
