62d3b4445ac8c8553aa4b3e8705025dc591e37b3
RiteshDhongade <ritesh_d37@yahoo.com>
2023-08-10 23:55:03 +0530

    Static_method

--- Static_Method.py
+++ Static_Method.py
@@ -1,1 +1,25 @@
 #Staic_Method
+class pwskills:
+    def student_details(self , name , mail_id , number) :
+        print(name , mail_id , number)
+
+pw = pwskills()
+pw.student_details("rit" , "rit@gmail.com" , 97979797)
+
+class pwskills1:
+    def student_details(self , name , mail_id , number):
+        print(name , mail_id , number)
+
+    @staticmethod
+    def mentor_class(list_mentor) :
+        print(list_mentor)
+
+    def mentor(self , mentor_list) :
+        print(mentor_list)
+
+pw1 = pwskills1()
+
+pw1.mentor(["rit" , "D37"])
+
+pw1.mentor_class(["Sum" , "Sud"])
+
